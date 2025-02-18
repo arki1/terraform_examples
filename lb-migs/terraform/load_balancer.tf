722ae0d35150e51006b60e5224aee29afd9ee19b
Feu <fabricio@feu.com.br>
2025-02-18 12:32:39 -0300

    adding SSL

--- lb-migs/terraform/load_balancer.tf
+++ lb-migs/terraform/load_balancer.tf
@@ -17,8 +17,8 @@ resource "google_compute_url_map" "url_map" {
 }
 
 # HTTP Proxy
-resource "google_compute_target_https_proxy" "http_proxy" {
-  name    = "http-proxy"
+resource "google_compute_target_https_proxy" "https_proxy" {
+  name    = "https-proxy"
   url_map = google_compute_url_map.url_map.id
   ssl_certificates = [google_compute_managed_ssl_certificate.ssl_cert.id]
 }
