0659f8ce9c7c9494b1e0a22f977453c14c858d3f
Feu <fabricio@feu.com.br>
2025-02-19 14:28:25 -0300

    simplifying declaration

--- lb-migs/terraform/main.tf
+++ lb-migs/terraform/main.tf
@@ -21,38 +21,6 @@ resource "google_project_iam_member" "storage_access" {
   member  = "serviceAccount:${google_service_account.vm_service_account.email}"
 }
 
-module "gce-container" {
-  source = "terraform-google-modules/container-vm/google"
-  version = "~> 3.2"
-
-  container = {
-    image="gcr.io/${var.project_id}/container-hello:latest"
-    securityContext = {
-      privileged : true
-    }
-    tty : true
-
-    # container has automatic access to host ports
-    env = [
-      {
-        name = "GOOGLE_CLOUD_REGION"
-        value = var.region
-      }
-    ]
-
-    volumeMounts = []
-  }
-
-  volumes = []
-
-  restart_policy = "Always"
-}
-
-data "google_compute_image" "gce_container_vm_image" {
-  family  = "cos-stable"
-  project = "cos-cloud"
-}
-
 # VM Template with Built-in Container Support
 resource "google_compute_instance_template" "vm_template" {
   name_prefix  = "vm-template-"
@@ -60,15 +28,31 @@ resource "google_compute_instance_template" "vm_template" {
 
   # Container-Optimized OS (cos-stable)
   disk {
-    source_image      = data.google_compute_image.gce_container_vm_image.self_link
+    source_image      = {
+      family  = "cos-stable"
+      project = "cos-cloud"
+    }
     auto_delete       = true
     boot              = true
     disk_type         = "pd-balanced"
     disk_size_gb      = 10
   }
 
+  # container has automatic access to host ports
+  # see: https://cloud.google.com/compute/docs/containers/configuring-options-to-run-containers#publishing_container_ports
   metadata = {
-    "gce-container-declaration" = module.gce-container.metadata_value
+    "gce-container-declaration" = <<-EOT
+      spec:
+        containers:
+        - name: container-hello
+          image: gcr.io/${var.project_id}/container-hello
+          env:
+          - name: GOOGLE_CLOUD_REGION
+            value: ${var.region}
+          ports:
+          - containerPort: 80
+        restartPolicy: Always
+    EOT
   }
 
   labels = {
