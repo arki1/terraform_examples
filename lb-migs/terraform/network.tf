6d7d4599e300538d6388ccc2f2cb1a3ed659ac6a
Feu <fabricio@feu.com.br>
2025-02-19 15:12:48 -0300

    fix on multi region

--- lb-migs/terraform/network.tf
+++ lb-migs/terraform/network.tf
@@ -12,7 +12,7 @@ resource "google_compute_router" "nat_router" {
 resource "google_compute_router_nat" "cloud_nat" {
   for_each    = toset(var.regions)
   name                               = "cloud-nat"
-  router                             = google_compute_router.nat_router.name
+  router                             = google_compute_router.nat_router[each.value].name
   region                             = each.value
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
